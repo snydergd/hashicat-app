518a8ac1585586637bd4d4aeddc68598b96b8175
snydergd <snydergd91@gmail.com>
2024-01-09 20:52:48 +0000

    Remove module

--- main.tf
+++ main.tf
@@ -205,5 +205,5 @@ resource "null_resource" "configure-cat-app" {
 module "azure-backup" {
   source              = "app.terraform.io/snydergd-tfc-azure/azure-backup/azurerm"
   version             = "1.0.2"
-  resource_group_name = "${var.prefix}-private"
+  resource_group_name = "${var.prefix}-public"
 }
